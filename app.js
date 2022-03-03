8f8e3b6f148c7a68575c3608b3ca8957793efd0b
h-sander <hsanderlourenco@gmail.com>
2022-03-03 17:39:51 -0300

    add start script

--- app.js
+++ app.js
@@ -159,6 +159,8 @@ async function main() {
     });
 }
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log('Listening on port ' + port);
 });
